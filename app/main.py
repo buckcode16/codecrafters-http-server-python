c942d734ab5b7b4ab8cfa59fd452573f6b9b3638
Bucky <buckcode16@gmail.com>
2024-02-10 23:19:50 +0800

    pass stage 2

--- app/main.py
+++ app/main.py
@@ -3,13 +3,14 @@ import socket
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    client_socket, client_address = server_socket.accept()
+    print(f"Connnection from {client_address} has been established.")
+
+    message = client_socket.recv(1024).decode("utf-8")
+    print(f"Received from client: {message}")
+
+    client_socket.sendall("HTTP/1.1 200\r\n\r\n".encode("utf-8"))
 
 
 if __name__ == "__main__":
