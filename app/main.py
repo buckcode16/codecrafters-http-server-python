a2d1e515e4b98b53958cc26232bc8a399051d13c
Bucky <buckcode16@gmail.com>
2024-02-11 01:33:43 +0800

    <pass stage 4>

--- app/main.py
+++ app/main.py
@@ -1,25 +1,25 @@
 import socket
 
-
 def main():
     HOST = 'localhost'
     PORT = 4221
-    with socket.create_server((HOST, PORT), reuse_port=True) as server_socket:
+    while True:
         print(f"Server is listening on {HOST},{PORT}")
 
-        client_socket, client_address = server_socket.accept()
-        print(f"Connnection from {client_address} has been established.")
+        with socket.create_server((HOST, PORT), reuse_port=True) as server_socket:
+            client_socket, client_address = server_socket.accept()
+            print(f"Connnection from {client_address} has been established.")
 
-        with client_socket:
-            message = client_socket.recv(1024).decode("utf-8")
-            print(f"Received from client: {message}")
+            with client_socket:
+                message = client_socket.recv(1024).decode("utf-8")
+                print(f"Received from client: {message}")
 
-            path = extract_path(message)
-            if(path != None):
-                if(path == '/'):
-                    send_response(client_socket)
+                path = extract_path(message)
+
+                if(path == '/' or path.startswith('/echo/')):
+                    send_response(client_socket,path)
                 else:
-                    send_response(client_socket, body="Not Found", status_code=404)
+                    send_response(client_socket,path, status_code = 404)
 
 
 def extract_path(http_request):
@@ -31,15 +31,43 @@ def extract_path(http_request):
     parts = request_line.split(' ')
 
     path = parts[1] if len(parts) > 1 else None
+    
     return path
 
-def send_response(client_socket, body='', content_type="text/html; charset=UTF-8", status_code=200, status_message="OK"):
+def extract_last_string(http_request):
+
+    lines = http_request.split('\n')
+
+    request_line = lines[0]
+
+    parts = request_line.split(' ')
+
+    path = parts[1] if len(parts) > 1 else None
+    
+    random_string = path.split('/')
+
+    extract = random_string[-1] if len(random_string) > 1 else None
+    return extract
+
+def send_response(client_socket, path, status_code = 200):
+
+    content_type = "text/html; charset=UTF-8"
+    body = ''
+    status_message = "OK"
+
+    if path.startswith('/echo/'):
+        content_type = "text/plain"
+        body = path[len('/echo/'):]
+    
+    if (status_code == 404):
+        status_message = "Not Found"
 
     response_body = body.encode("utf-8")
+    content_length = len(response_body)
+
     response_headers = f"HTTP/1.1 {status_code} {status_message}\r\n"\
                       f"Content-Type: {content_type}\r\n"\
-                      f"Content-Length: {len(response_body)}\r\n"\
-                      f"Connection: close\r\n\r\n"
+                      f"Content-Length: {content_length}\r\n\r\n"
     response = response_headers.encode("utf-8") + response_body
 
     client_socket.sendall(response)
---
+++ app/playground.py
@@ -0,0 +1,7 @@
+def extract_string(str):
+    arr = str.split('/')
+
+    print(arr)
+
+if __name__ == '__playground.py__':
+    extract_string('/horse/coo')
